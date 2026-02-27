d1b0472967b16aeb0f27ccd9fd1dabd4a423fe78
Shahzaib <shazuthebigbrother@gmail.com>
2026-02-27 17:20:23 +0500

    not foundpage del

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 import MoreReviewdata from "./pages/MoreReviewsPage.tsx"
 import TopGamesPage from "./pages/TopGamesPage.tsx"
 import AboutPage from "./pages/AboutPage.tsx"
 import ContactPage from "./pages/ContactPage.tsx"
-
 
 
 export default function App() {
@@ -21,7 +19,6 @@ export default function App() {
           <Route path="/topgamespage" element={<TopGamesPage />} />  
           <Route path="/aboutpage" element={<AboutPage />} />  
           <Route path="/contactPage" element={<ContactPage />} />  
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
